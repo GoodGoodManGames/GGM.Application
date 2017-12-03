2d81bcad4221e0b299cbf8cf614aa38bfe5de3ad
= <=>
2017-12-03 21:00:23 +0900

    Initialize project, Seperate application classes from ggmcontext

---
+++ GGMApplication/GGMApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using GGM.Context;
+
+namespace GGM.Application
+{
+    public class GGMApplication
+    {
+        private GGMApplication(string[] args)
+        {
+            Arguments = args;
+        }
+
+        //TODO: 추후 객체화 될 예정
+        public string[] Arguments { get; }
+
+        public Assembly ApplicationAssembly { get; private set; }
+        public ManagedContext Context { get; private set; }
+
+        public static ManagedContext Run(Type applicationClass, string[] args)
+        {
+            return new GGMApplication(args).Run(applicationClass);
+        }
+
+        private ManagedContext Run(Type applicationClass)
+        {
+            ApplicationAssembly = applicationClass.Assembly;
+            Context = new ApplicationContext(ApplicationAssembly);
+            return Context;
+        }
+    }
+}
