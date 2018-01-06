757f05ecc8e98734250d969451f3ebd056d54799
goodgoodman <gjwnsan9420@gmail.com>
2018-01-07 00:23:24 +0900

    resolved #5 유닛 테스트 작성

--- GGMApplicationTest/GGMApplicationTest.cs
+++ GGMApplicationTest/GGMApplicationTest.cs
@@ -7,32 +7,46 @@ namespace GGMApplicationTest
 {
     public class GGMApplicationTest
     {
-        public ITestOutputHelper Output { get; }
-
-        public GGMApplicationTest(ITestOutputHelper output)
+        public GGMApplicationTest()
         {
-            Output = output;
+            _application = GGMApplication.Run(
+                GetType()
+                , "../../../application.cfg"
+                , new string[] { }
+                , typeof(ConstructorService)
+                , typeof(NonConstructorService)
+            );
         }
+
+        GGMApplication _application;
 
         [Fact]
         public void ServiceTest()
         {
-            var application = GGMApplication.Run(GetType(), "../../../application.cfg", new string[] { }
-            , typeof(ConstructorService)
-            , typeof(NonConstructorService)
-            );
-            Assert.NotNull(application);
-            Assert.NotEmpty(application.Services);
-            Assert.Equal(application.Services.Count, 2);
-            Assert.True(application.Services[0] is ConstructorService);
-            Assert.True(application.Services[1] is NonConstructorService);
+            Assert.NotNull(_application);
+            Assert.NotEmpty(_application.Services);
+            Assert.Equal(_application.Services.Count, 2);
+            Assert.True(_application.Services[0] is ConstructorService);
+            Assert.True(_application.Services[1] is NonConstructorService);
         }
 
-        //[Fact]
-        //public void NonServiceTest()
-        //{
-        //    var application = GGMApplication.Run(this.GetType(), "", new string[] { });
-        //    Assert.NotNull(application);
-        //}
+        
+        ////// application.cfg's body/////////
+        // project.name=GGMApplication Test //
+        // project.testint = 100000         //
+        // project.testlong = 10000000000   //
+        // project.testfloat = 3.3          //
+        // project.testdouble = 3.31232451  //
+        //////////////////////////////////////
+        [Fact]
+        public void ConfigMappingTest()
+        {
+            var service = _application.Services[0] as ConstructorService;
+            Assert.Equal(service.ApplicationName, "GGMApplication Test");
+            Assert.Equal(service.TestInt, 100000);
+            Assert.Equal(service.TestLong, 10000000000);
+            Assert.Equal(service.TestFloat, 3.3f);
+            Assert.Equal(service.TestDouble, 3.31232451d);
+        }
     }
 }
