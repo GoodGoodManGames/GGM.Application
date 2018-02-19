670d6eb964f98a18f4d8381efb699117063f1ef5
goodgoodman <gjwnsan9420@gmail.com>
2018-02-19 22:29:43 +0900

    GGM.Context 버전업과 그에 의한 수정.

--- GGM.Application/ApplicationContext.cs
+++ GGM.Application/ApplicationContext.cs
@@ -26,33 +26,53 @@ namespace GGM.Application
         public ApplicationContext(Assembly assembly, Dictionary<string, string> configs)
         {
             Assembly = assembly;
-            mConfigs = configs;
+            _configs = configs;
 
-            var allTypes = Assembly.GetTypes();
-            var managedTypes = allTypes
-                .Where(type => type.IsDefined(typeof(ManagedAttribute)))
+            // 모든 어셈블리의 Type을 탐색하여 Managed를 등록
+            var allAssemblyManagedTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(item => item.GetTypes())
+                .Where(type => type.IsDefined(typeof(ManagedAttribute)));
+            foreach (var type in allAssemblyManagedTypes)
+                Register(type);
+
+            // 현재 어셈블리의 Singleton들은 미리 생성.
+            var currentAssemblySignletonTypes = allAssemblyManagedTypes
+                .Where(type => type.Assembly == assembly)
                 .ToDictionary(type => type, type => type.GetCustomAttribute<ManagedAttribute>());
-
-            // Singleton들은 미리 생성.
-            foreach (var managedType in managedTypes)
+            foreach (var managedType in currentAssemblySignletonTypes)
             {
-                // Key : type
-                // Value : ManagedAttribute
                 if (managedType.Value.ManagedType == ManagedType.Singleton)
                     GetManaged(managedType.Key);
             }
+        }
+        
+        static private IEnumerable<Type> Explore(Assembly assembly)
+        {
+            return assembly.GetTypes().SelectMany(ApplicationContext.Explore);
+        }
+
+        static private IEnumerable<Type> Explore(Type type)
+        {
+            foreach (var _type in type.GetNestedTypes(BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly).SelectMany(ApplicationContext.Explore)) { yield return _type; }
+            yield return type;
+        }
+
+        static private IEnumerable<Type> Explore()
+        {
+            var _domain = AppDomain.CurrentDomain.GetAssemblies();
+            return _domain.SelectMany(ApplicationContext.Explore);
         }
 
         /// <summary>
         ///     Scan한 Assembly
         /// </summary>
         public Assembly Assembly { get; }
-        private Dictionary<string, string> mConfigs;
-        private Dictionary<Type, ConfigMapper> mConfigMapperCache = new Dictionary<Type, ConfigMapper>();
+        private Dictionary<string, string> _configs;
+        private Dictionary<Type, ConfigMapper> _configMapperCache = new Dictionary<Type, ConfigMapper>();
 
-        public override object Create(Type type, object[] parameters = null)
+        public override object Create(BaseManagedDefinition definition, object[] parameters = null)
         {
-            var result = base.Create(type, parameters);
+            var result = base.Create(definition, parameters);
 
             //TODO: 해당 type에 ConfigAttribute가 없는 경우 아래 일련의 동작을 하지 않으면 속도 항상을 얻을 수 있다.
             //      Singleton인 경우는 성능 누락이 의미없겠지만, Proto의 경우에는 다름.
@@ -66,10 +86,10 @@ namespace GGM.Application
         internal void ConfigMapping(object target)
         {
             var type = target.GetType();
-            if (!mConfigMapperCache.ContainsKey(type))
-                mConfigMapperCache[type] = GenerateConfigMapper(type);
+            if (!_configMapperCache.ContainsKey(type))
+                _configMapperCache[type] = GenerateConfigMapper(type);
 
-            var configMapper = mConfigMapperCache[type];
+            var configMapper = _configMapperCache[type];
             configMapper(target);
         }
 
@@ -84,7 +104,7 @@ namespace GGM.Application
             foreach (var configInfo in configInfos)
             {
                 var key = configInfo.ConfigAttribute.Key;
-                if (!mConfigs.ContainsKey(key))
+                if (!_configs.ContainsKey(key))
                 {
                     Console.WriteLine("해당 Key의 설정이 존재하지 않습니다. 이는 무시됩니다.");
                     continue;
@@ -93,7 +113,7 @@ namespace GGM.Application
                 var propertyInfo = configInfo.PropertyInfo;
                 var propertyType = propertyInfo.PropertyType;
 
-                var value = mConfigs[key];
+                var value = _configs[key];
                 il.Emit(Ldarg_0); // [targetObject]
                 #region [targetObject] [Value]
                 if (propertyInfo.PropertyType == typeof(string))
